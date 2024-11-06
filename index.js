6a7a489c626fc2144fd802b1ef7de1a60693282c
xmrsv <88881293+xmrsv@users.noreply.github.com>
2024-11-05 21:12:48 -0500

    usar puerto de proveedor

--- index.js
+++ index.js
@@ -112,6 +112,6 @@ app.delete("/reservas/:id", async (req, res) => {
 	}
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is running on port 3000");
 });
